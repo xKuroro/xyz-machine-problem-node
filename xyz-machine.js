465ed1c53c7fdfc29158a5d5b7b7dc0fa5542bb8
Lucilfer <bennington12323@gmail.com>
2022-07-01 10:00:19 +0800

    no more unnecessary consoles.

--- xyz-machine.js
+++ xyz-machine.js
@@ -2,38 +2,29 @@
 console.log('it works')
 const ps = require('prompt-sync')
 const prompt1 = ps();
-const prompt2 = ps()
-const name = require('./tools/classInput');
-const validation = require('./tools/validation');
-const printXYZ = require('./tools/printXYZ');
+const prompt2 = ps();
+const validation = require("./tools/validation");
+const printXYZ = require("./tools/printXYZ");
 
+const inputNumber = parseInt(prompt1("Enter odd number above 2: "));
+const inputText = prompt2("Enter letter X,Y and Z: ").trim().toUpperCase();
+const inputDirection = prompt1("Enter Direction Vertical or Horizontal: ")
+  .trim()
+  .toUpperCase();
 
-prompt1.toString()
-console.log(name.inputs.inputsText)
-// const names = prompt('enter name: ');
-// console.log(names)
+let ff = validation.validation(inputNumber, inputText, inputDirection);
+let splitText = inputText.split("");
+console.log(splitText);
 
+if (!ff) {
+  console.log("input text field correctly.");
+} else {
+  let arrayPrint = printXYZ(inputNumber, splitText);
 
-    const inputNumber = parseInt(prompt1('Enter odd number above 2: '))
-    const inputText = prompt2('Enter letter X,Y and Z: ').trim().toUpperCase();
-    const inputDirection = prompt1('Enter Direction Vertical or Horizontal: ').trim().toUpperCase();
-
-   
-  let ff = validation.validation(inputNumber,inputText,inputDirection);
-  let splitText = inputText.split('');
-  console.log(splitText)
-
-  if(!ff){
-    console.log('input text field correctly.')
-  }else{
-    // let vertical = '';
-    // let temp = '';
-    let arrayPrint = printXYZ(inputNumber, splitText);
-
-    if (inputDirection && inputDirection === "HORIZONTAL") {
-      console.log("This is horizontal");
-      console.log(arrayPrint);
-    } else {
-      console.log(arrayPrint);
-    }
+  if (inputDirection && inputDirection === "HORIZONTAL") {
+    console.log("This is horizontal");
+    console.log(arrayPrint);
+  } else {
+    console.log(arrayPrint);
   }
+}
